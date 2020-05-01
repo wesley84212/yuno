d7268f19bc0e105e2128ddac8292cce193905aa9
Liou <a8532582@gmail.com>
2020-05-01 22:37:34 +0800

    0501

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Header() {
+    return (
+        <div>
+            <div className="header_logo">
+                
+            </div>
+            <div className="header_btn">
+
+            </div>
+        </div>
+    )
+}
