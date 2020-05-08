f1e7ec423f276544d7252e847387ac720280186b
a8532582 <a8532582@gmail.com>
2020-05-08 17:07:01 +0800

    0508

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <Router>
     <Switch>
-
-
-
+    <Route path="/shop">
+    </Route>
+    <Route path="/about">
+    </Route>
     <Route path="/">
     </Route>
     </Switch>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from "react";
+import { NavLink } from 'react-router-dom';
 
 export default function NavBar() {
-    return (
-        <div>
-            
-
-
-        </div>
-    )
+  return (
+    <div>
+        <NavLink to=""></NavLink>
+        <NavLink to=""></NavLink>
+        <NavLink to=""></NavLink>
+        <NavLink to=""></NavLink>
+    </div>
+  );
 }
