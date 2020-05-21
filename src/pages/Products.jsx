988dfe3caecae454bc83b70d3929f1075a55b06b
LiouShuYu <a8532582@mds.com.tw>
2020-05-21 16:52:23 +0800

    0521

---
+++ src/pages/Products.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+function Products(){
+
+    const SingleProduct=(<>
+    <div>
+        
+        <div>
+            <img src="" alt=""/>
+        </div>
+        <h4>商品標題</h4>
+        <p>商品簡介</p>
+
+        <div>
+            <div className="cart"></div>
+            <div className="heart"></div>
+        </div>
+
+    </div>
+    </>)
+
+    return(<>
+    
+    
+    
+    
+    
+    </>)
+
+}
+
+export default Products()
