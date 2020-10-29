9216863c698a26e758d1f505cf835845f7e13808
Wesley Hsieh <wesley.hsieh@dlinkcorp.com>
2020-10-29 18:13:52 +0800

    Change: add electron

--- src/pages/Inventory.jsx
+++ src/pages/Inventory.jsx
@@ -1,55 +1,63 @@
 import React, { useState, useEffect } from 'react'
 
 
-function inventory() {
+function Inventory() {
 
-    // const [productList, setProductList] = useState([])
+    const [productList, setProductList] = useState({ purchase: [] })
 
-    // async function getDataFromServer() {
-    //     const request = new Request(
-    //         'http://192.168.1.106:3000/purchase',
-    //         {
-    //             method: 'GET',
-    //             mode: 'cors',
-    //             credentials: 'include',
-    //             headers: new Headers({
-    //                 Accept: 'application/json',
-    //                 'Content-Type': 'appliaction/json',
-    //             }),
-    //         }
-    //     )
+    async function getDataFromServer() {
+        const request = new Request(
+            'http://localhost:3000/purchase',
+            {
+                method: 'GET',
+                mode: 'cors',
+                credentials: 'include',
+                headers: new Headers({
+                    Accept: 'application/json',
+                    'Content-Type': 'appliaction/json',
+                }),
+            }
+        )
 
-    //     const response = await fetch(request)
-    //     const data = await response.json()
-    //     setProductList(data)
+        const response = await fetch(request)
+        const data = await response.json()
+        setProductList(data)
+    }
 
-    // }
-
-    // useEffect(() => {
-    //     getDataFromServer()
-    // }, [])
-    // console.log('productlist', productlist)
-
-
-
-    // const products = (<>
-    //     {productlist.purchase.map((value, index) => {
-    //         return (
-    //             <tr>
-    //                 <td className="products-table w-10p">{value.id}</td>
-    //                 <td className="products-table">{value.product.name}</td>
-    //                 <td className="products-table w-10p">{value.cost}</td>
-    //                 <td className="products-table w-15p">{value.purchaseDate}</td>
-    //                 <td className="products-table w-15p"></td>
-    //             </tr>
-    //         )
-    //     })}
-    // </>)
+    useEffect(() => {
+        getDataFromServer()
+    }, [])
+    let incomes = 0;
+    let costs = 0;
+    let saleCharges = 0;
+    let saleAmounts = 0;
+    let products = (<>
+        {productList && productList.purchase.map((value, index) => {
+            incomes += value.income;
+            costs += value.cost;
+            saleCharges += value.saleCharge;
+            saleAmounts += value.saleAmount;
+            const saleDate = value.saleDate === "0000-00-00" ? "" : value.saleDate;
+            return (
+                <tr>
+                    <td className="products-table w-10p">{value.id}</td>
+                    <td className="products-table">{value.product.name}</td>
+                    <td className="products-table w-10p">{value.cost}</td>
+                    <td className="products-table w-15p">{value.purchaseDate}</td>
+                    <td className="products-table w-15p">{saleDate}</td>
+                </tr>
+            )
+        })}
+    </>)
 
 
     return (<>
         <div className="m-0auto">
             <h3 className="mt-2 mb-2">存貨列表</h3>
+            <span>總銷售額{saleAmounts}</span>/
+            <span>總淨利{incomes}</span>/
+            <span>總交易費用{saleCharges}</span>/
+            <span>總進貨成本{costs}</span>
             <table>
                 <tr>
                     <th className="products-table w-10p">商品編號</th>
@@ -58,7 +66,7 @@ function inventory() {
                     <th className="products-table w-15p">進貨日期</th>
                     <th className="products-table w-15p">售出日期</th>
                 </tr>
-
+                {products}
             </table>
 
 
@@ -70,4 +78,4 @@ function inventory() {
 
 }
 
-export default inventory
+export default Inventory
